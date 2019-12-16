017a4ff851d45b83abd8f2c0c09a506fb650cc03
sondt <sondt1610@gmail.com>
2019-12-16 08:08:07 +0700

    sds

--- js/index.js
+++ js/index.js
@@ -1,47 +1,51 @@
 $(document).ready(function(){
-    // $('.owl-carousel').owlCarousel({
-    //     loop:true,
-    //     items:1,
-    //     // autoplay: true
-    // })
-
-    $("#NavDrawer .drawer__header .drawer__close button").click(function () {
-        $("#NavDrawer").removeClass("open");
-        $(".cart-overlay").removeClass("open");
+    // hide mobile menu
+    $("#nav_drawer .drawer_header .drawer_close button").click(function () {
+        $("#nav_drawer").removeClass("open");
+        $(".body_overlay").removeClass("open");
     });
-    $(".click-menu").click(function () {
-        $("#NavDrawer").addClass("open");
-        $(".cart-overlay").addClass("open");
+    // show mobile menu
+    $(".icon_show_mobile_menu").click(function () {
+        $("#nav_drawer").addClass("open");
+        $(".body_overlay").addClass("open");
     });
+    // show sub menu
     $(".icon-plus").click(function () {
         $(this).parent().parent().addClass("open");
         $(this).parent().find('.icon-minus').css({ "display": "inline" });
         $(this).css({ "display": "none" });
     });
+    // hide sub menu
     $(".icon-minus").click(function () {
         $(this).parent().parent().removeClass("open");
         $(this).css({ "display": "none" });
         $(this).parent().find('.icon-plus').css({ "display": "inline" });
     });
 
+    //  show slider
+    // start index of first slide item is 0;
     var slideIndex = 0;
-    showSlides();
-
+    // slides is array of slide items
+    var slides = document.getElementsByClassName("slide_item");
+    if(slides.length !== 0){
+        showSlides();
+    }
     function showSlides() {
         var i;
-        var slides = document.getElementsByClassName("mySlides");
         var dots = document.getElementsByClassName("dot");
+        // hide all slides
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
         slideIndex++;
         if (slideIndex > slides.length) { slideIndex = 1 }
+        // assign all dots is unactive
         for (i = 0; i < dots.length; i++) {
-            console.log(dots[i].className.replace(" active", ""));
             dots[i].className = dots[i].className.replace(" active", "");
         }
+        // assign index of current slide item
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
-        // setTimeout(showSlides, 8000); // Change image every 8 seconds
+        setTimeout(showSlides, 8000); // Change image every 8 seconds
     }
 });
---
+++ template/js/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    // hide mobile menu
+    $("#nav_drawer .drawer_header .drawer_close button").click(function () {
+        $("#nav_drawer").removeClass("open");
+        $(".body_overlay").removeClass("open");
+    });
+    // show mobile menu
+    $(".icon_show_mobile_menu").click(function () {
+        $("#nav_drawer").addClass("open");
+        $(".body_overlay").addClass("open");
+    });
+    // show sub menu
+    $(".icon-plus").click(function () {
+        $(this).parent().parent().addClass("open");
+        $(this).parent().find('.icon-minus').css({ "display": "inline" });
+        $(this).css({ "display": "none" });
+    });
+    // hide sub menu
+    $(".icon-minus").click(function () {
+        $(this).parent().parent().removeClass("open");
+        $(this).css({ "display": "none" });
+        $(this).parent().find('.icon-plus').css({ "display": "inline" });
+    });
+
+    //  show slider
+    // start index of first slide item is 0;
+    var slideIndex = 0;
+    // slides is array of slide items
+    var slides = document.getElementsByClassName("slide_item");
+    if(slides.length !== 0){
+        showSlides();
+    }
+    function showSlides() {
+        var i;
+        var dots = document.getElementsByClassName("dot");
+        // hide all slides
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        // assign all dots is unactive
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        // assign index of current slide item
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 8000); // Change image every 8 seconds
+    }
+});

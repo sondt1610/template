fb583bf61e56e24a6c2d125deadc509b0a4ae1fc
sondt <sondt1610@gmail.com>
2019-12-22 23:37:24 +0700

    dfd

--- js/index.js
+++ js/index.js
@@ -46,6 +46,6 @@ $(document).ready(function(){
         // assign index of current slide item
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
-        setTimeout(showSlides, 8000); // Change image every 8 seconds
+        // setTimeout(showSlides, 8000); // Change image every 8 seconds
     }
 });
